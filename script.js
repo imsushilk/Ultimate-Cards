6c100ae178bb5b642846b6d0524df975f2f49344
imsushilk <50072103+imsushilk@users.noreply.github.com>
2026-01-08 14:16:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector('.stack').addEventListener('click', () => {
+/*document.querySelector('.stack').addEventListener('click', () => {
   document.querySelectorAll('.card').forEach((card, i) => {
     setTimeout(() => {
       card.className = 'card';
@@ -12,4 +12,20 @@ document.querySelector('.spread').addEventListener('click', () => {
       card.className = 'card ani' + i;
     }, i * 150);
   });
-});
+});*/
+
+document.querySelector('.card').addEventListener('click', () => {
+  document.querySelectorAll('.card').forEach((card, i) => {
+    setTimeout(() => {
+      card.className = 'card ani' + i + '  anicard';
+    }, i * 150);
+  });
+})
+
+document.querySelector('.anicard').addEventListener('click', () => {
+  document.querySelectorAll('.anicard').forEach((card, i) => {
+    setTimeout(() => {
+      card.className = 'card';
+    }, i * 150);
+  });
+})
