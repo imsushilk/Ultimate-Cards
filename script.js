05d9cceeb3cbbda9f7721147702e8185065be300
imsushilk <misushilkumar@gmail.com>
2026-01-08 19:07:42 +0530

    Updated animation

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-document.querySelectorAll('.defcard').forEach((card) => {
-  card.addEventListener('click', () => {
-    if (card.className.includes('ani')) {
-      document.querySelectorAll('.defcard').forEach((card, i) => {
+document.querySelectorAll(".defcard").forEach((card) => {
+  card.addEventListener("click", () => {
+    if (card.className.includes("ani")) {
+      document.querySelectorAll(".defcard").forEach((card, i) => {
         setTimeout(() => {
-          card.className = 'defcard card';
+          card.className = "defcard card";
         }, i * 150);
       });
     } else {
-      document.querySelectorAll('.defcard').forEach((card, i) => {
+      document.querySelectorAll(".defcard").forEach((card, i) => {
         setTimeout(() => {
           card.className = `defcard card ani${i}`;
         }, i * 150);
       });
-      document.querySelectorAll('.defcard').forEach((card, i) => {
-        setTimeout(() => {
-          card.className = 'defcard card';
-        }, i * 450);
-      });
+      setTimeout(() => {
+        document.querySelectorAll(".defcard").forEach((card, i) => {
+          setTimeout(() => {
+            card.className = "defcard card";
+          }, i * 2 * 150);
+        });
+      }, 2000);
     }
   });
 });
