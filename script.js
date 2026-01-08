257e8f8a8370a30a15c69aa4f48922e191d16da7
imsushilk <50072103+imsushilk@users.noreply.github.com>
2026-01-08 15:24:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,18 +14,20 @@ document.querySelector('.spread').addEventListener('click', () => {
   });
 });*/
 
-document.querySelector('.card').addEventListener('click', () => {
-  document.querySelectorAll('.card').forEach((card, i) => {
-    setTimeout(() => {
-      card.className = 'card ani' + i + '  anicard';
-    }, i * 150);
+document.querySelectorAll('.defcard').forEach((card) => {
+  card.addEventListener('click', () => {
+    if (card.classList.contains('ani')) {
+      document.querySelectorAll('.defcard').forEach((card, i) => {
+        setTimeout(() => {
+          card.className = 'defcard card';
+        }, i * 150);
+      });
+    } else {
+      document.querySelectorAll('.defcard').forEach((card, i) => {
+        setTimeout(() => {
+          card.className = `defcard card ani${i}`;
+        }, i * 150);
+      });
+    }
   });
-})
-
-document.querySelector('.anicard').addEventListener('click', () => {
-  document.querySelectorAll('.anicard').forEach((card, i) => {
-    setTimeout(() => {
-      card.className = 'card';
-    }, i * 150);
-  });
-})
+});
