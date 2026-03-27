934161dda1a29659b70ce9bbc7c1ea4841fc1c04
imsushilk <misushilkumar@gmail.com>
2026-03-27 21:31:12 +0530

    Updated pathing

--- js/utils.js
+++ js/utils.js
@@ -2,7 +2,8 @@
 export async function fetchProfileData() {
   const profileName = new URLSearchParams(window.location.search).get("profile");
   if (!profileName) return null;
-  const response = await fetch(`../profiles/${profileName}.json`);
+  const response = await fetch(`profiles/${profileName}.json`);
+  console.log(`Fetch response for "${profileName}":`, response);
   if (!response.ok) {
     console.error(`Failed to fetch profile data for "${profileName}": ${response.status}`);
     return null;
